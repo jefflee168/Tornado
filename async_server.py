8ab6d1b4fefd1b56ce96dce151326beaab3a1b32
jefflee168 <86116193@qq.com>
2018-12-14 18:18:14 +0800

    add file async__server.py

---
+++ async_server.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,time
+import datetime as dt
+
+import tornado.gen
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+class SleepHandler(tornado.web.RequestHandler):
+    def get(self):
+        time.sleep(5)
+	self.write("When I sleep")
+
+if __name__ == "__main__":
+    app = tornado.web.Application(
+	[
+	    (r"/sleep", SleepHandler)
+	]
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
