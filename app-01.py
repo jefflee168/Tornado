756db60fb1707908504d14b3cbf7afde8f637e1d
jefflee168 <1919081933@qq.com>
2018-12-23 21:36:17 +0800

    update app.py

---
+++ app-01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        #self.write("<h1>Hello world!</h1>")
+        self.render("base.html")
+
+class ErrorHandler(tornado.web.RequestHandler):
+    def get(self):
+	# 将变量 msg 传递给页面 500.html
+        self.render("500.html", msg="后台有错，请重试！")
+
+def make_app():
+    return tornado.web.Application(
+	[
+	    (r"/",MainHandler),
+	    (r"/500",ErrorHandler),
+        ],
+	template_path=os.path.join(
+	    os.path.dirname(__file__),"templates"
+	),
+	debug=True
+    )
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
